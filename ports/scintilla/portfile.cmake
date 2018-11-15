c5941cccaf01cc817abcc4b66cb9874f675cd2b2
Gordon Smith <gordonjsmith@gmail.com>
2018-11-15 06:19:50 +0000

    chore: Branch for ECLIDE
    Use Scintilla 3.7.6
    Use WTL 9.x
    Add GSoap
    Add BugTrap
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- ports/scintilla/portfile.cmake
+++ ports/scintilla/portfile.cmake
@@ -1,22 +1,57 @@
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  message(FATAL_ERROR "scintilla only supports dynamic linkage")
+endif()
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
+  message(FATAL_ERROR "scintilla only supports dynamic crt")
+endif()
+
 include(vcpkg_common_functions)
-
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
-
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}/scintilla)
 vcpkg_download_distfile(ARCHIVE
-  URLS "http://www.scintilla.org/scintilla412.zip"
-  FILENAME "scintilla412.zip"
-  SHA512 10e24a2def5b3104b0f2eec473c53edb0a0cc19fbbef261e460a77415ec68ff83f9ee20c76cda7987627708c1d4ead5f964d4d5a98929d8256280bfa9bd0cddc
+  URLS "http://www.scintilla.org/scintilla376.zip"
+  FILENAME "scintilla376.zip"
+  SHA512 618a50405eede3277d7696ac58122aeeb490d10ae392c60c7f78baaa96c965a8e1a599948e0ebd61bed7f75894b01bdf4574a0e5d0e20996bfdfb2e1bdb33203
 )
-vcpkg_extract_source_archive_ex(
-  OUT_SOURCE_PATH SOURCE_PATH
-  ARCHIVE ${ARCHIVE}
-  REF 4.1.2
+file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET})
+vcpkg_extract_source_archive(${ARCHIVE} ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET})
+
+if(TRIPLET_SYSTEM_ARCH MATCHES "x86")
+  set(BUILD_ARCH "Win32")
+else()
+  set(BUILD_ARCH ${TRIPLET_SYSTEM_ARCH})
+endif()
+
+vcpkg_build_msbuild(
+  PROJECT_PATH ${SOURCE_PATH}/Win32/SciLexer.vcxproj
+  PLATFORM ${MSBUILD_PLATFORM}
 )
 
-vcpkg_install_msbuild(
-  SOURCE_PATH ${SOURCE_PATH}
-  PROJECT_SUBPATH Win32/SciLexer.vcxproj
-  INCLUDES_SUBPATH include
-  LICENSE_SUBPATH License.txt
-  ALLOW_ROOT_INCLUDES
-)
+# Handle headers
+file(INSTALL ${SOURCE_PATH}/include/ILexer.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/include/Sci_Position.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/include/SciLexer.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/include/Scintilla.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+
+# Handle libraries
+if(BUILD_ARCH STREQUAL "Win32")
+  set(BUILD_DIR_DEBUG "/Debug")
+  set(BUILD_DIR_RELEASE "/Release")
+else()
+  set(BUILD_DIR_DEBUG "${BUILD_ARCH}/Debug")
+  set(BUILD_DIR_RELEASE "${BUILD_ARCH}/Release")
+endif()
+
+if(VCPKG_LIBRARY_LINKAGE MATCHES "dynamic")
+  file(INSTALL ${SOURCE_PATH}/win32/${BUILD_DIR_RELEASE}/SciLexer.dll  DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+  file(INSTALL ${SOURCE_PATH}/win32/${BUILD_DIR_DEBUG}/SciLexer.dll  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif()
+file(INSTALL ${SOURCE_PATH}/win32/${BUILD_DIR_RELEASE}/SciLexer.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(INSTALL ${SOURCE_PATH}/win32/${BUILD_DIR_DEBUG}/SciLexer.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+
+# Handle PDBs
+
+file(INSTALL ${SOURCE_PATH}/win32/${BUILD_DIR_RELEASE}/SciLexer.pdb  DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+file(INSTALL ${SOURCE_PATH}/win32/${BUILD_DIR_DEBUG}/SciLexer.pdb  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/scintilla RENAME copyright)
