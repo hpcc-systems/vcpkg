c31cd5eeea25ab50a6c30161472a687c58c0fde5
Gordon Smith <GordonJSmith@gmail.com>
2022-08-03 17:08:09 +0100

    fix:  Support all OS with older version of libtool
    Improved patching of openldap configure.ac
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- overlays/openldap/portfile.cmake
+++ overlays/openldap/portfile.cmake
@@ -4,59 +4,28 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 30fdc884b513c53169910eec377c2ad05013b9f06bab3123d50d028108b24548791f7f47f18bcb3a2b4868edeab02c10d81ffa320c02d7b562f2e8f2fa25d6c9
 )
 
-execute_process(COMMAND bash "-c" "awk -F= '/^NAME/{print $2}' \"/etc/os-release\""
-    OUTPUT_VARIABLE OS_RELEASE
-    ERROR_VARIABLE OS_RELEASE_ERROR
-    OUTPUT_STRIP_TRAILING_WHITESPACE
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    PATCHES
+        openssl.patch
 )
-message("OS_RELEASE (OS_RELEASE_ERROR): ${OS_RELEASE} (${OS_RELEASE_ERROR})")
 
-if(OS_RELEASE STREQUAL "\"CentOS Linux\"")
-    vcpkg_extract_source_archive_ex(
-        OUT_SOURCE_PATH SOURCE_PATH
-        ARCHIVE ${ARCHIVE}
-        PATCHES
-        openssl.patch
-        m4.patch
-    )
-else()
-    vcpkg_extract_source_archive_ex(
-        OUT_SOURCE_PATH SOURCE_PATH
-        ARCHIVE ${ARCHIVE}
-        PATCHES
-        openssl.patch
-    )
-endif()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    set(BUILD_OPTS --enable-shared=yes --enable-static=no)
-else()
-    set(BUILD_OPTS --enable-shared=no --enable-static=yes)
-endif()
+execute_process(
+    COMMAND autoreconf
+    WORKING_DIRECTORY ${SOURCE_PATH}
+)
 
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
-    AUTOCONFIG
-
-    # PRERUN_SHELL autoreconf --force --install
     OPTIONS
-    ${BUILD_OPTS}
-    --disable-slapd
-    --with-tls=openssl
-    --without-cyrus-sasl
-    "LIBS=-ldl"
-
-    # --enable-slapd
-    # --enable-modules
-    # --enable-rlookups
-    # --enable-backends=mod
-    # --disable-ndb
-    # --disable-sql
-    # --enable-overlays=mod
+        --disable-slapd
+        --with-tls=openssl
+        --without-cyrus-sasl
+        "LIBS=-ldl"
 )
 
-vcpkg_build_make(BUILD_TARGET depend)
-vcpkg_build_make()
+vcpkg_build_make(BUILD_TARGET depend LOGFILE_ROOT depend)
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
