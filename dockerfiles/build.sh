b9cba616c4f25daa93ef3880f1d5d6f6c4788a87
Gordon Smith <GordonJSmith@gmail.com>
2023-07-12 09:16:31 +0100

    fix:  Remove cacheing
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -8,6 +8,7 @@ export $(grep -v '^#' $SCRIPT_DIR/../.env | xargs -d '\r' | xargs -d '\n') > /de
 GITHUB_ACTOR="${GITHUB_ACTOR:-hpcc-systems}"
 GITHUB_TOKEN="${GITHUB_TOKEN:-none}"
 GITHUB_REF=$(git rev-parse --short=8 HEAD)
+GITHUB_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DOCKER_USERNAME="${DOCKER_USERNAME:-hpccbuilds}"
 DOCKER_PASSWORD="${DOCKER_PASSWORD:-none}"
 
@@ -15,26 +16,29 @@ echo "SCRIPT_DIR: $SCRIPT_DIR"
 echo "GITHUB_ACTOR: $GITHUB_ACTOR"
 echo "GITHUB_TOKEN: $GITHUB_TOKEN"
 echo "GITHUB_REF: $GITHUB_REF"
+echo "GITHUB_BRANCH: $GITHUB_BRANCH"
 echo "DOCKER_USERNAME: $DOCKER_USERNAME"
 echo "DOCKER_PASSWORD: $DOCKER_PASSWORD"
 
 # docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 function doBuild() {
-    docker build --progress plain --pull --rm -f "$SCRIPT_DIR/$1.dockerfile" \
-        -t hpccsystems/platform-build-base-$1:$GITHUB_REF \
-        -t hpccsystems/platform-build-base-$1:latest \
+    docker build --progress plain -f "$SCRIPT_DIR/$1.dockerfile" \
         --build-arg GITHUB_ACTOR=$GITHUB_ACTOR \
         --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
+        -t hpccsystems/platform-build-base-$1:$GITHUB_REF \
+        -t hpccsystems/platform-build-base-$1:$GITHUB_BRANCH \
         "$SCRIPT_DIR/.."
+
     # docker push hpccsystems/platform-build-base-$1:$GITHUB_REF
     # docker push hpccsystems/platform-build-base-$1:latest
 }
 
-doBuild amazonlinux
-doBuild ubuntu-22.10
-# doBuild ubuntu-22.04
-doBuild ubuntu-20.04
-doBuild ubuntu-18.04
-doBuild centos-8
-doBuild centos-7
+doBuild centos-7 &
+doBuild centos-8 &
+doBuild amazonlinux &
+doBuild ubuntu-22.10 &
+doBuild ubuntu-22.04 &
+doBuild ubuntu-20.04 &
+
+wait
