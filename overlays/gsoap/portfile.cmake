7676761e67f0750f2e162d8861640d7455e331d7
Gordon Smith <GordonJSmith@gmail.com>
2025-10-13 18:09:45 +0100

    feat: bump gsoap to latest version
    Added -z6 flag to (mostly) generate compatible stubs.
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- overlays/gsoap/portfile.cmake
+++ overlays/gsoap/portfile.cmake
@@ -1,12 +1,10 @@
-# Header-only library
-
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH CURRENT_BUILDTREES_DIR
     REPO hpcc-systems/gsoap
     REF v${VERSION}
-    SHA512 b2e1c52d25cbd461a6f22308d3da4095465ea4c6ecbbe25005225d22aa978e82d49dc54f6b2e9bf3702b542e68a35f75c082956669a3e1ceb9e6bcb499353f94
+    SHA512 47cb6b382737edab18f00d4adb62dd9dd7ef9fbe16f46e173fed9861fea4cb516660cab865be46adcc6a81947aa9faac709d9b4c8a0367828413a04a42143d9b
     HEAD_REF master
 )
 
@@ -15,33 +13,3 @@ file(COPY ${CURRENT_BUILDTREES_DIR}/gsoap DESTINATION ${CURRENT_PACKAGES_DIR}/sh
 file(COPY ${CURRENT_BUILDTREES_DIR}/gsoap/stdsoap2.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/gsoap)
 
 file(INSTALL ${CURRENT_BUILDTREES_DIR}/license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/gsoap RENAME copyright)
-
-
-
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
-#include(vcpkg_common_functions)
-#set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/gsoap_2.7.13)
-#vcpkg_download_distfile(ARCHIVE
-    #URLS "https://sourceforge.net/projects/gsoap2/files/gsoap-2.7/gsoap_2.7.13.zip/download"
-    #FILENAME "gsoap_2.7.13.zip"
-    #SHA512  9d10fde082ac1b20cbf0713f813d118e60bc5b3a9136d99915959a06c8e67aa9983524c0c18a26fc0430a63d82c5c5d83717f8437c1433c2068f2ee34de40ccf
-#)
-#vcpkg_extract_source_archive(${ARCHIVE})
-
-#file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include ${CURRENT_PACKAGES_DIR}/src)
-
-#file(COPY ${CURRENT_BUILDTREES_DIR}/src/gsoap-2.7/gsoap/stdsoap2.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/gsoap)
-
-#file(INSTALL ${CURRENT_BUILDTREES_DIR}/src/gsoap-2.7/license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/gsoap RENAME copyright)
-
