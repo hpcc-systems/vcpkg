aa84aea1d3512ef7c65019891ae157110d3a1f1b
Gordon Smith <GordonJSmith@gmail.com>
2026-05-25 09:23:14 +0100

    fix: update Dockerfiles to install Node.js 22.x and clean up package lists
    Migrate overlays back into vcpkg submodule
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- overlays/openldap/portfile.cmake
+++ overlays/openldap/portfile.cmake
@@ -27,7 +27,7 @@ else()
 endif()
 
 if(VCPKG_TARGET_IS_ANDROID)
-    vcpkg_list(APPEND FEATURE_OPTIONS -with-yielding_select=yes)
+    vcpkg_list(APPEND FEATURE_OPTIONS --with-yielding_select=yes)
 elseif(VCPKG_TARGET_IS_EMSCRIPTEN)
     vcpkg_list(APPEND FEATURE_OPTIONS --with-yielding_select=no)
 endif()
@@ -55,9 +55,49 @@ vcpkg_make_configure(
 )
 
 vcpkg_make_install(TARGETS depend install)
+
+# Normalize usr/local-prefixed install trees into vcpkg package roots.
+set(_openldap_usr_local "${CURRENT_PACKAGES_DIR}/usr/local")
+if(IS_DIRECTORY "${_openldap_usr_local}")
+    foreach(_dir bin include lib share)
+        if(IS_DIRECTORY "${_openldap_usr_local}/${_dir}")
+            file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/${_dir}")
+            file(COPY "${_openldap_usr_local}/${_dir}/" DESTINATION "${CURRENT_PACKAGES_DIR}/${_dir}")
+            file(REMOVE_RECURSE "${_openldap_usr_local}/${_dir}")
+        endif()
+    endforeach()
+endif()
+
+# Some autotools installs place pkg-config files under usr/local.
+# Move them to locations vcpkg_fixup_pkgconfig() checks.
+set(_openldap_pc_dir "${CURRENT_PACKAGES_DIR}/usr/local/lib/pkgconfig")
+if(IS_DIRECTORY "${_openldap_pc_dir}")
+    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
+    file(GLOB _openldap_pc_files "${_openldap_pc_dir}/*.pc")
+    if(_openldap_pc_files)
+        file(COPY ${_openldap_pc_files} DESTINATION "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
+        file(REMOVE ${_openldap_pc_files})
+    endif()
+endif()
+
+if(NOT VCPKG_BUILD_TYPE)
+    set(_openldap_pc_dir_dbg "${CURRENT_PACKAGES_DIR}/debug/usr/local/lib/pkgconfig")
+    if(IS_DIRECTORY "${_openldap_pc_dir_dbg}")
+        file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
+        file(GLOB _openldap_pc_files_dbg "${_openldap_pc_dir_dbg}/*.pc")
+        if(_openldap_pc_files_dbg)
+            file(COPY ${_openldap_pc_files_dbg} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
+            file(REMOVE ${_openldap_pc_files_dbg})
+        endif()
+    endif()
+endif()
+
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
